2cdc8d643b7ff516457eec424920c8c61215c14a
marty <martin.philip.jacobs@gmail.com>
2019-02-18 18:50:53 +1000

    get interface info

---
+++ index.js
@@ -0,0 +1,28 @@
+
+'use strict';
+
+var os = require('os');
+var ifaces = os.networkInterfaces();
+
+exports.printMsg = function() {
+
+  Object.keys(ifaces).forEach(function (ifname) {
+    var alias = 0;
+
+    ifaces[ifname].forEach(function (iface) {
+      if ('IPv4' !== iface.family || iface.internal !== false) {
+        // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+        return;
+      }
+
+      if (alias >= 1) {
+        // this single interface has multiple ipv4 addresses
+        return ifaces;
+      } else {
+        // this interface has only one ipv4 adress
+        return ifaces;
+      }
+      ++alias;
+    });
+  });
+}
